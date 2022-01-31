ef2eea3786c9e8cf4d4b4018b9ccea2375586f3e
thaisfinck <thaisfinck@gmail.com>
2022-01-31 16:22:02 +0700

    login connection with the database

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 // Import Packages
 const express = require('express')
 const router = express.Router()
+const Users = require('../models/users')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -12,7 +13,25 @@ router.get('/signup', (req, res) => {
 
 router.post('/login', (req, res) => {})
 
-router.post('/signup', (req, res) => {})
+router.post('/signup', async (req, res, next) => {
+  try {
+    let existingUser = await Users.findOne({ email: req.body.email })
+    if (existingUser) {
+      throw new Error('User already exist!')
+    } else {
+      let user = await Users.create(req.body)
+      req.login(user, err => {
+        if (err) {
+          throw err
+        } else {
+          res.redirect('/')
+        }
+      })
+    }
+  } catch (err) {
+    next(err)
+  }
+})
 
 router.get('/logout', (req, res) => {})
 
